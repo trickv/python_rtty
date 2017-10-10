8fa663ad684e1ff4eb2d8271be80b0850daaf012
Patrick van Staveren <trick@vanstaveren.us>
2017-10-10 21:37:08 +0000

    add simple testing on script

---
+++ on.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import wiringpi
+import time
+
+wiringpi.wiringPiSetupGpio()
+
+enable_pin = 23
+red_pin = 4
+green_pin = 17
+wiringpi.pinMode(enable_pin, 1) # mode output
+wiringpi.pinMode(red_pin, 1) # mode output
+wiringpi.pinMode(green_pin, 1) # mode output
+wiringpi.digitalWrite(enable_pin,1)
+wiringpi.digitalWrite(red_pin,1)
+wiringpi.digitalWrite(green_pin,1)
